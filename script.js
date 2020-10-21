ce241b67cba4eb8b4fb8a2d56f1e9f66126c6a48
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-21 17:21:31 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $("#nav").load("nav.html");
+
+    function activeNav() {
+        var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+         $("#nav ul li a").each(function(){
+              if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+              $(this).addClass("active");
+         });
+    }
+
+    setTimeout(function() {
+        activeNav();
+    }, 100);
+
+});
